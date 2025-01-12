c8cdb573c7d25a68bbeaf447066521ded9baa481
Markus <markussmadsbakken@gmail.com>
2025-01-12 21:15:22 +0100

    add pagewrapper to events and use react query instead of fetch

--- app/(tabs)/arrangementer/index.tsx
+++ app/(tabs)/arrangementer/index.tsx
@@ -3,6 +3,9 @@ import EventCard from "@/components/ui/eventCard";
 import { Text } from "@/components/ui/text";
 import { router } from "expo-router";
 import { View, ScrollView } from "react-native";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
+import PageWrapper from "@/components/ui/pagewrapper";
 
 type Event = {
     id: string;
@@ -14,78 +17,82 @@ type Event = {
 };
 
 export default function Arrangementer() {
-    const [events, setEvents] = useState<Event[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
-    const [page, setPage] = useState(1);
     const resultsPerPage = 10;
 
-    const fetchEvents = async () => {
-        try {
-            setLoading(true);
-            setError(null);
-
-            const queryParams = new URLSearchParams({
-                page: page.toString(),
-                None: resultsPerPage.toString(),
-            });
-
-            const response = await fetch(`https://api.tihlde.org/events/?${queryParams}`);
-            if (!response.ok) {
-                throw new Error(`Feil ved henting av arrangementer: ${response.statusText}`);
-            }
-
-            const data = await response.json();
-            setEvents((prevEvents) => [...prevEvents, ...data.results]);
-        } catch (err) {
-            if (err instanceof Error) {
-                setError(err.message);
-            } else {
-                setError("En ukjent feil oppstod");
-            }
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        fetchEvents();
-    }, [page]);
-
-    const loadMoreEvents = () => {
-        setPage((prevPage) => prevPage + 1);
-    };
-
-    if (loading && page === 1) {
-        return <Text className="text-center mt-10 text-lg text-gray-600">Laster arrangementer...</Text>;
+    const fetchEvents = async ({ pageParam }: { pageParam: number }): Promise<{ results: Event[] }> => {
+        const queryParams = new URLSearchParams({
+            page: pageParam.toString(),
+            None: resultsPerPage.toString(),
+        });
+        const res = await fetch(`https://api.tihlde.org/events/?${queryParams}`)
+        return res.json();
     }
 
-    if (error) {
-        return <Text className="text-center mt-10 text-lg text-red-500">Feil: {error}</Text>;
+    const {
+        data,
+        error,
+        fetchNextPage,
+        hasNextPage,
+        isFetching,
+        isFetchingNextPage,
+        status,
+    } = useInfiniteQuery({
+        queryKey: ["events"],
+        queryFn: fetchEvents,
+        initialPageParam: 1,
+        getNextPageParam: (lastPage, allPages, lastPageParam) => {
+            if (!lastPage.results || lastPage.results?.length === 0) {
+                return undefined;
+            }
+            return lastPageParam + 1;
+        },
+    })
+
+    if (status === "pending") {
+        <Text className="text-center mt-10 text-lg text-gray-600">Laster arrangementer...</Text>;
+    }
+
+    if (status === "error") {
+        return <Text className="text-center mt-10 text-lg text-red-500">Feil: {error.message}</Text>;
     }
 
     return (
-        <ScrollView className="p-10 shadow-lg rounded-lg mt-10">
+        <PageWrapper className="px-10" refreshQueryKey={"events"}>
             <Text className="text-2xl font-bold text-center mb-6">Arrangementer</Text>
-            {events.map((event) => (
-                <EventCard
-                    key={event.id}
-                    id={event.id}
-                    title={event.title}
-                    date={new Date(event.start_date)}
-                    image={event.image || null}
-                    onPress={() => router.push(`/arrangementer/${event.id}`)}
-                />
-            ))}
-            {loading && <Text className="text-center mt-4 text-lg text-gray-600">Laster flere arrangementer...</Text>}
-            {!loading && (
-                <Text
-                    onPress={loadMoreEvents}
-                    className="text-center text-lg text-blue-500 underline mt-4"
-                >
-                    Last flere arrangementer
-                </Text>
-            )}
-        </ScrollView>
+
+            {data?.pages.map((group, i) => (
+                < React.Fragment key={i} >
+                    {Array.isArray(group?.results) &&
+                        group.results.map((event) => (
+                            <EventCard
+                                key={event.id}
+                                id={event.id}
+                                title={event.title}
+                                date={new Date(event.start_date)}
+                                image={event.image || null}
+                                onPress={() => router.push(`/arrangementer/${event.id}`)}
+                            />
+                        ))
+                    }
+                </React.Fragment>
+            ))
+            }
+            <View className="mb-16">
+                {isFetchingNextPage && <Text className="text-center mt-4 text-lg text-gray-600">Laster flere arrangementer...</Text>}
+                {
+                    (hasNextPage && !isFetchingNextPage) && (
+                        <Button
+                            onPress={() => fetchNextPage()}
+                            variant={"link"}
+                        >
+                            <Text>
+                                Last flere arrangementer
+                            </Text>
+                        </Button>
+                    )
+                }
+                {!hasNextPage && <Text className="text-center mt-4 text-lg text-gray-600">Ingen flere arrangementer</Text>}
+            </View>
+        </PageWrapper >
     );
 }
