dec51afa4b02e8a2ec138a7e860940932db47593
Mads Nylund <madsnyl@gmail.com>
2025-01-24 19:18:07 +0100

    fixed color palette

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary web:hover:opacity-90 active:opacity-90',
+        default: 'bg-primary dark:bg-[#1C5ECA] web:hover:opacity-90 active:opacity-90',
         destructive: 'bg-destructive web:hover:opacity-90 active:opacity-90',
         outline:
           'border border-input bg-background web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent',
