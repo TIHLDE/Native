020599e8ae6a7ab5eed2ad210c2d5dc1c581c7ba
Markus <markussmadsbakken@gmail.com>
2025-03-04 12:14:34 +0100

    Remove qrmodal from profile layout

--- app/profil/_layout.tsx
+++ app/profil/_layout.tsx
@@ -4,15 +4,14 @@ export default function karriereLayout() {
 
     return (
         <Stack>
-            <Stack.Screen 
-            name="index" 
-            options={{ 
-                headerShown: false, 
-                title: "Profil" 
+            <Stack.Screen
+                name="index"
+                options={{
+                    headerShown: false,
+                    title: "Profil"
                 }}
-                
-                />
-            <Stack.Screen name="qrmodal" options={{ presentation: "modal", title: "Qr-kode" }} />
+
+            />
             <Stack.Screen name="eventmodal" options={{ presentation: "card" }} />
         </Stack>
     )
