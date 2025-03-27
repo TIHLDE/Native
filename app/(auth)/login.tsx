3631de003efa418a1b2bc820dc7a5f549e8cc057
Alivki <iverlindholm@icloud.com>
2025-03-27 20:02:11 +0100

    redesign of login page

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,20 +1,29 @@
-import login, { LoginInput } from "@/actions/auth/login";
-import { ApiError, LoginData } from "@/actions/types";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Text } from "@/components/ui/text";
-import { useAuth } from "@/context/auth";
+import login, {LoginInput} from "@/actions/auth/login";
+import {ApiError, LoginData} from "@/actions/types";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {Text} from "@/components/ui/text";
+import {useAuth} from "@/context/auth";
 import TihldeLogo from "@/lib/icons/TihldeLogo";
-import { useMutation } from "@tanstack/react-query";
-import { useRouter } from "expo-router";
-import { useState } from "react";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import {useMutation} from "@tanstack/react-query";
+import {useRouter} from "expo-router";
+import {useState} from "react";
+import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 import Toast from "react-native-toast-message";
-import { View, KeyboardAvoidingView, Platform, ScrollView, TouchableWithoutFeedback, Keyboard } from "react-native";
+import * as WebBrowser from 'expo-web-browser';
+import {
+    View,
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView,
+    TouchableWithoutFeedback,
+    Keyboard,
+    Linking
+} from "react-native";
 
 
 export default function Login() {
-    const { setAuthState } = useAuth();
+    const {setAuthState} = useAuth();
     const router = useRouter();
 
     const [email, setEmail] = useState<string>("");
@@ -46,27 +55,46 @@ export default function Login() {
         }
     });
 
-    const onPress = () => mutate({ email, password });
+    const onPress = () => mutate({email, password});
+
+    const _handlePressButtonForgotPasswordAsync = async () => {
+        await WebBrowser.openBrowserAsync('https://tihlde.org/glemt-passord');
+    };
+
+    const _handlePressButtonCreateUserAsync = async () => {
+        await WebBrowser.openBrowserAsync('https://tihlde.org/ny-bruker');
+    };
 
     return (
         <SafeAreaProvider>
             <KeyboardAvoidingView
                 behavior={Platform.OS === "ios" ? "padding" : "height"}
-                style={{ flex: 1 }}
+                style={{flex: 1}}
             >
                 <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                    <SafeAreaView className="flex-1 gap-y-8 px-4 justify-center">
+                    <SafeAreaView className="flex-1 gap-y-8 py-2 justify-center">
                         <ScrollView
-                            contentContainerStyle={{ flexGrow: 1, justifyContent: "flex-start", gap: 15 }}
+                            contentContainerStyle={{flexGrow: 1, justifyContent: "flex-start", gap: 15}}
                             keyboardShouldPersistTaps="handled"
+                            className={"px-10"}
                         >
                             <View className="gap-y-0 items-center w-full mt-32 mb-16">
                                 <TihldeLogo size="large" className="color-white"/>
                             </View>
-                            <View className="gap-y-2">
-                                <Text>Brukernavn</Text>
+
+                            <View className={"gap-y-0 px-5 mb-2"}>
+                                <Text className="text-2xl mb-2 font-semibold">
+                                    Logg inn
+                                </Text>
+                                <Text>
+                                    Logg inn med ditt TIHLDE-brukernavn og passord
+                                </Text>
+                            </View>
+
+                            <View className="gap-y-2 mb-10 px-5">
                                 <Input
-                                    className="w-full"
+                                    placeholder={"Brukernavn"}
+                                    className="w-full bg-card border-gray-400 focus:border-black transition duration dark:focus:border-white dark:border-gray-300 rounded-none border-t-0 border-l-0 border-r-0 border-b-1"
                                     value={email}
                                     onChangeText={setEmail}
                                     autoCapitalize="none"
@@ -74,10 +102,10 @@ export default function Login() {
                                 />
                             </View>
 
-                            <View className="gap-y-2">
-                                <Text>Passord</Text>
+                            <View className="gap-y-2 px-5 mb-2">
                                 <Input
-                                    className="w-full"
+                                    placeholder={"Passord"}
+                                    className="w-full bg-card border-gray-400 focus:border-black transition duration-200 dark:focus:border-white dark:border-gray-300 rounded-none border-t-0 border-l-0 border-r-0"
                                     value={password}
                                     onChangeText={setPassword}
                                     secureTextEntry
@@ -85,19 +113,35 @@ export default function Login() {
                                 />
                             </View>
 
-                            <Button
-                                onPress={onPress}
-                                disabled={!email || !password || status === "pending"}
-                                className="w-full mt-4"
+                            <View className="px-5 mb-1">
+                                <Button
+                                    onPress={onPress}
+                                    disabled={!email || !password || status === "pending"}
+                                    className="w-full mt-4 "
+                                    size={"lg"}
+                                >
 
-                            >
-                                <Text>
-                                    {status === "pending"
-                                        ? "Logger inn..."
-                                        : "Logg inn"
-                                    }
-                                </Text>
-                            </Button>
+                                    <Text>
+                                        {status === "pending"
+                                            ? "Logger inn..."
+                                            : "Logg inn"
+                                        }
+                                    </Text>
+                                </Button>
+                            </View>
+
+                            <View className="flex flex-row justify-between px-5">
+                                <Button variant={"link"} className={""} onPress={_handlePressButtonForgotPasswordAsync} >
+                                    <Text className="dark:text-accent">
+                                        Glemt passord?
+                                    </Text>
+                                </Button>
+                                <Button variant={"link"} className={""} onPress={_handlePressButtonCreateUserAsync} >
+                                    <Text className="dark:text-accent">
+                                        Opprett bruker
+                                    </Text>
+                                </Button>
+                            </View>
                         </ScrollView>
                     </SafeAreaView>
                 </TouchableWithoutFeedback>
